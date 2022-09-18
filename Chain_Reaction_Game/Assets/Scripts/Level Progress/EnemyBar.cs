ec2823f61d24eab1a5ad4bcbcdfdbc94906dcaa0
Jiazhi Chang <59265236+jiazhiprojects@users.noreply.github.com>
2022-09-17 18:47:35 -0700

    Clean up

--- Chain_Reaction_Game/Assets/Scripts/Level Progress/EnemyBar.cs
+++ Chain_Reaction_Game/Assets/Scripts/Level Progress/EnemyBar.cs
@@ -18,7 +18,6 @@ public class EnemyBar : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log("On Collision Enter");
         TakeDamage(2);
         if (health <= 0)
         {
